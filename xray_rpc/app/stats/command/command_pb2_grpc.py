f66fe406af6121523f784e26566b754483d62561
Denis Sokolov <denis.sokolov@leroymerlin.ru>
2025-12-30 02:03:29 +0300

    fix path

--- xray_rpc/app/stats/command/command_pb2_grpc.py
+++ xray_rpc/app/stats/command/command_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import command_pb2 as command__pb2
+from xray_rpc.app.stats.command import command_pb2 as command__pb2
 
 GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
